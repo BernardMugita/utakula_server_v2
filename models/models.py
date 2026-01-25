8bd832660272cebf1208cf1ebfdeb02dbdb6daa9
Bernard Mugita <mugita68@gmail.com>
2026-01-25 23:08:11 +0300

    feat: models update

--- models/models.py
+++ models/models.py
@@ -2,10 +2,3 @@ from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     pass
-
-from models.user_model import UserModel
-from models.calorie_model import CalorieModel
-from models.food_model import FoodModel
-from models.meal_plan_model import MealPlanModel
-from models.notification_model import NotificationModel
-from models.user_metrics_model import UserMetricsModel
